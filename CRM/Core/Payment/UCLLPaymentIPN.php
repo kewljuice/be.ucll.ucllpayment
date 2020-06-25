c4d5b94ad253bedda55bab3761b927e9e722dadb
wouter.hechtermans <wouter.hechtermans@calibrate.be>
2020-06-25 15:49:41 +0200

    refs #payment added basic webhook logic.

--- CRM/Core/Payment/UCLLPaymentIPN.php
+++ CRM/Core/Payment/UCLLPaymentIPN.php
@@ -53,10 +53,45 @@ class CRM_Core_Payment_UCLLPaymentIPN extends CRM_Core_Payment_BaseIPN {
    * This method is handles the response that will be invoked
    */
   static function main() {
-    // @todo handle IPN.
-    $variables = print_r(json_decode(file_get_contents('php://input'), TRUE), TRUE);
-    echo $variables;
-    Civi::log()->debug($variables);
+    // Fetch POST variables.
+    $variables = json_decode(file_get_contents('php://input'), TRUE);
+    if (isset($variables['verificationId']) && isset($variables['status'])) {
+      $params = [
+        'invoice_id' => $variables['verificationId'],
+        'is_test' => ['IN' => [0, 1]],
+      ];
+      // Check if given verificationId matches a contribution.
+      try {
+        $check = civicrm_api3('Contribution', 'getSingle', $params);
+      } catch (\CiviCRM_API3_Exception $e) {
+        \Civi::log()->debug("UCLLPaymentIPN.php: " . $e->getMessage());
+      }
+      if (!$check['is_error'] && isset($check['contact_id'])) {
+        unset($params['is_test']);
+        $params['contribution_id'] = $check['contribution_id'];
+        $params['contact_id'] = $check['contact_id'];
+        $params['total_amount'] = $check['total_amount'];
+        $params['financial_type_id'] = $check['financial_type_id'];
+        // Contribution status.
+        switch ($variables['status']) {
+          case 'success':
+            $params['contribution_status_id'] = "Completed";
+            break;
+          default:
+            $params['contribution_status_id'] = "Cancelled";
+        }
+        // Add shoppingCartHash as trxn_id.
+        if (isset($variables['shoppingCartHash'])) {
+          $params['trxn_id'] = $variables['shoppingCartHash'];
+        }
+        try {
+          // Update contribution.
+          civicrm_api3('Contribution', 'create', $params);
+        } catch (\CiviCRM_API3_Exception $e) {
+          \Civi::log()->debug("UCLLPaymentIPN.php: " . $e->getMessage());
+        }
+      }
+    }
   }
 
 }
